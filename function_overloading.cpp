784c6c72b7244de5e028a8d442e10896212354c6
varlamovdima <varlamovdima@gmail.com>
2022-03-20 12:53:26 +0100

    function overloading

---
+++ function_overloading.cpp
@@ -0,0 +1,29 @@
+// This program shows off overloaded functions in C++ 
+// By: Dmitrii Varlamov
+#include <iostream>
+using namespace std;
+
+void print(char c){
+	cout<<"char size is "<<sizeof(c)<<endl;
+}
+void print(int i){
+	cout<<"int size is "<<sizeof(i)<<endl;
+}
+void print(float f){
+	cout<<"float size is "<<sizeof(f)<<endl;
+}
+void print(double d){
+	cout<<"double size is "<<sizeof(d)<<endl;
+}
+
+int main(){
+	char c = 23;
+	int i = 52;
+	float f = 12.3;
+	double d = 332.2;
+	print(c);
+	print(i);
+	print(f);
+	print(d);
+	return 0;
+}
